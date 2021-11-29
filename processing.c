649c353b5ab8d085b7500641e88d0441b700e49e
Guedouah Khaled <khaledguedouah2000@gmail.com>
2021-11-29 16:13:49 +0100

    PN

--- processing.c
+++ processing.c
@@ -119,6 +119,17 @@ for (x=1;x<=n;x++ ){
     }
     return a;
 }
+double Pn(double x, int n){
+    if(n==0){
+        return 1;
+    }else if(n==1){
+        return x ;
+    }else{
+        return (double)((2*n-1)*x*Pn(x,n-1)-(n-1)*Pn(x,n-2))/n;
+    }
+}
+
+
 
 
 
