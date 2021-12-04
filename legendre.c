a9faee72f83de909107b37bbd689894935ceee14
ZAIDI Mohamed <zaidimedh@gmail.com>
2021-12-05 00:22:38 +0100

    edit branch modeling

--- legendre.c
+++ legendre.c
@@ -69,3 +69,81 @@ double Norm_Const(int p, int q)
 
     return C;
 }
+double Moment_Leg (BmpImg img,int p,int q, int n )  // Tested OK
+{
+    int i,j;
+    double res=0.00;
+    double c = Norm_Const(p, q);
+    for (i=0 ; i<=p ; i++){
+        for (j=0 ; j<=q; j++ ){
+            res += coeff (p,i)*coeff (q,j)*mom_geo_centre (img,p, q, n)   ;
+        }
+    }
+    res*= c ;
+    return res ;
+}
+double ** Moments_Legendre (BmpImg img, int n ) // Tested OK 
+{
+    double ** mat = creer_mat_anti_diag(n);
+    int p,q;
+    for (p=0 ; p<n ; p++ ){
+        for (q=0 ; q<n-p;q++){
+            mat[p][q]=Moment_Leg (img, p,q,n);
+        }
+    }
+    return mat ;
+}
+void ecrire_mom (char * filename ,double ** moments ,int n ) // Tested OK
+{
+int i , j;
+FILE * fichier = fopen (filename,"w");
+if ( fichier != NULL ) {
+    fprintf(fichier , "%d \n" , n);
+    for (i=0 ; i< n ; i++ ){
+        for (j=0 ; j<n-i ; j++){
+            fprintf(fichier , "%f " , moments[i][j]);
+        }
+        fprintf(fichier, "\n");
+    }
+}
+else {printf("Error Opening File Please Fix the problem and retry");} 
+fclose(fichier);
+}
+
+double ** lire_moments(char * filename ){ // Tested OK 
+int n, i , j;
+FILE * fichier = fopen (filename,"r");
+if ( fichier != NULL ) {
+fscanf(fichier , "%d \n" , &n);
+double ** mat = creer_mat_anti_diag(n);
+float tmp ; // Does not work without this temporary Variable
+for (i=0 ; i< n ; i++ ){
+    for (j=0 ; j<n-i ; j++){
+        fscanf(fichier , "%f" , &tmp);
+        mat[i][j]=tmp;
+    }
+    fscanf(fichier, "\n");
+    }
+return mat ;
+}
+else {printf("Error Opening File Please Fix the problem and retry");
+fclose (fichier);
+
+
+return NULL ;
+}
+}
+
+double Dist_Euc (double ** mat1 , double **mat2 , int n ) {  // tested with moments calculated from same image 
+                                                             //except that one of them is saved and red (in/from) a file 
+int p,q;                                                     // Result was 0.000004 Due to the use of the tmp variable 
+double res = 0.00 ;                                          // in the function <lire_moments>
+    for (p=0 ; p<n ; p++ ){
+        for (q=0 ; q<n-p;q++){
+            res+= pow(mat1[p][q]-mat2[p][q],2);
+        }
+    }
+res = sqrt (res);
+
+return res;
+}
--- legendre.h
+++ legendre.h
@@ -4,5 +4,12 @@
 #include "myBmpGris.h"
 double coeff (int x, int i);
 double ** coeff_legendre (int n);
+double Dist_Euc (double ** mat1 , double **mat2 , int n ); // tested with moments calculated from same image 
+double ** lire_moments(char * filename );
+void ecrire_mom (char * filename ,double ** moments ,int n );
+double ** Moments_Legendre (BmpImg img, int n );
+double Moment_Leg (BmpImg img,int p,int q, int n );
+double Norm_Const(int p, int q);
+double P(double x,int n );
 
 #endif // LEGENDRE_H_INCLUDED
